2f121e4dc5c3b004b0807a5a7a90181b5ebc7c80
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-12-16 19:22:29 -0300

    Atualizacao no codigo da aula 7

--- UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat_MainClass.java
+++ UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat_MainClass.java
@@ -9,5 +9,8 @@ public class UltraEmojiCombat_MainClass {
 		l[0] = new Lutador("Pretty boy", "França", 31, 2, 4, 20, 1.75f, 68.9f);
 		l[0].status();
 		
+		l[1] = new Lutador("SnapShoot", "Brazil", 56, 1, 6, 21, 1.80f, 68.9f);
+		l[1].status();
+		
 	}
 }
