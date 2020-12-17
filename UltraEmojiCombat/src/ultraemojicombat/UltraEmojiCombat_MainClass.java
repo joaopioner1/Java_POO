fa1bdd0edc0adbdf0964d0c528f249d092e5c2da
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-12-16 22:49:03 -0300

    Adc outro lutador.

--- UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat_MainClass.java
+++ UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat_MainClass.java
@@ -6,11 +6,13 @@ public class UltraEmojiCombat_MainClass {
 		
 		Lutador l[] = new Lutador[6];
 			
-		l[0] = new Lutador("Pretty boy", "França", 31, 2, 4, 20, 1.75f, 68.9f);
+		l[0] = new Lutador("Pretty boy", "FranÃ§a", 31, 2, 4, 20, 1.75f, 68.9f);
 		l[0].status();
 		
-		l[1] = new Lutador("SnapShoot", "Brazil", 56, 1, 6, 21, 1.80f, 68.9f);
+		l[1] = new Lutador("SnapShoot", "Brasil", 56, 1, 6, 21, 1.80f, 68.9f);
 		l[1].status();
 		
+		l[2] = new Lutador("InstaGhost", "Alemanha", 41, 2, 3, 23, 1.79f, 85.4f);
+		l[2].status();
 	}
 }
