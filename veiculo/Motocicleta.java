1334b3a07ed4f7a7473f363acf7ed02d0c8ab692
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-12-27 18:39:17 -0300

    Adc a classe motocicleta terminada

--- veiculo/Motocicleta.java
+++ veiculo/Motocicleta.java
@@ -4,5 +4,10 @@ public class Motocicleta extends Veiculo {
 	
 	public void empinar() {
 		
+		if (this.getLigar()) {
+			System.out.println("Empinando! Cuidado para não cair.");
+		} else {
+			System.out.println("Impossível empinar, moto desligada.");
+		}
 	}
 }
