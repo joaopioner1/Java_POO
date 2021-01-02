a3b9af701efb1a1feabe41fb2c6e6e348e6fbb53
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-01-02 18:36:26 -0300

    Adc o programa Veiculo (outra forma de fazer)

--- veiculo2/MainClassVeiculo.java
+++ veiculo2/MainClassVeiculo.java
@@ -21,3 +21,8 @@ public class MainClassVeiculo {
 	}
 
 }
+/* I made this program because I wanted to practice some of the topics I learned yesterday.
+ * It's been really nice studying programming. Now it is much more productive than it was a few months ago.
+ * Let me explain my program: I made a class named Veiculo. This class is the abstract class, so I put all the methods there.
+ * those other classes are Veiculo's sons. I code some methods about what a car does; like open its doors. 
+ */
