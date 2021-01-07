5e929d336ed163340f3f7da6bb4609677ea56a9a
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-01-07 16:59:30 -0300

    Adc outra class

---
+++ CKFight/src/ckfight/Trucan.java
@@ -0,0 +1,48 @@
+package ckfight;
+
+public class Trucan extends Lutadores {
+
+	@Override
+	public void esquivar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void golpeCritico() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void golpe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void usarEspecial() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void carregarEnergia() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void acabarLuta() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mostrarStatus() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
