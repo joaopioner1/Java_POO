f1015c5f1da256a8da0afe7f34d4cb5c15eacc2a
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-01-12 17:18:22 -0300

    Last commit for today

--- CKFight/src/ckfight/Luta.java
+++ CKFight/src/ckfight/Luta.java
@@ -2,8 +2,8 @@ package ckfight;
 
 public class Luta {
 	
-	private boolean ativa, restrita;
-	private Lutadores l1, l2;
+	private boolean ativa, restrita, aprovada;
+	private Lutadores desafiante, desafiado;
 	
 	public void terminarLuta() {
 		this.setAtiva(false);
@@ -14,7 +14,15 @@ public class Luta {
 	}
 	
 	public void marcarLuta(Lutadores l1, Lutadores l2) {
-		
+		if (l1.getClassificado() == true && l2.getClassificado() && l1.getVivo() == true & l2.getVivo() == true) {
+			this.setAprovada(true);
+			this.desafiado = l2;
+			this.desafiante = l2;
+		} else {
+			this.setAprovada(false);
+			this.desafiado = null;
+			this.desafiante = null;
+		}
 	}
 	
 	public boolean getRestrita() {
@@ -32,5 +40,12 @@ public class Luta {
 	public void setAtiva(boolean ativa) {
 		this.ativa = ativa;
 	}
-	
+
+	public boolean getAprovada() {
+		return aprovada;
+	}
+
+	public void setAprovada(boolean aprovada) {
+		this.aprovada = aprovada;
+	}
 }
